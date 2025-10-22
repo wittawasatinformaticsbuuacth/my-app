d3d3206ee2addc657a49d770f04612e427a1e9dc
wittawasatinformaticsbuuacth <wittawas@informatics.buu.ac.th>
2025-10-22 12:56:31 +0700

    deploy

--- frontend/config.js
+++ frontend/config.js
@@ -1,11 +1,11 @@
 // ⚙️ Configuration สำหรับ API URL
 
 // สำหรับ Development (รันบนเครื่องตัวเอง)
-const API_URL = "http://localhost:3000/api";
+// const API_URL = "http://localhost:3000/api";
 
 // สำหรับ Production (deploy แล้ว)
 // เปลี่ยน URL นี้เป็น URL ที่ deploy backend ของคุณ
-// const API_URL = "https://my-app-backend.onrender.com/api";
+const API_URL = "https://my-app-backend-8ajw.onrender.com/";
 
 // หรือถ้า deploy บน VPS
 // const API_URL = 'http://YOUR_SERVER_IP:3000/api';
